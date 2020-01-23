6b8c7f86d4eb6f1d1bc3a0931a2c46d3b542296e
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-23 01:07:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let num1 = 31;
 let num2 = 2;
 let mulitply = 2 * 31;
-let random = Math.floor(Math.random());
+let random = Math.floor(Math.random() + 1);
 let mod = 12 % 8;
 let max = Math.min(1, 2, 3, 4, 20);
 // Write your code here
